9a8a24984107afe12fcebaf812fb03ddd752e193
Brandan Sayarath and Todd Walraven <todd@>
2020-01-23 09:57:12 -0800

    turn input from form into an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
     $("form.form").submit(function(event) {
         event.preventDefault();
+        var sentence = $("input#pig-latin").val().split("");
+        console.log(sentence);
+
+
+        
         $(".results").show();
-        $("")
+        $(".results").append("<p>" + sentence +"</p>")
     })
 })
